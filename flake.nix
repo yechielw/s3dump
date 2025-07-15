c5359d28ee205611ef9c4a6ca58f39e75c31eec0
Yechiel <41305372+yechielw@users.noreply.github.com>
2025-07-15 15:25:53 +0300

    Fix Nix flake build

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "s3dump Python utility";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python3;
+      in rec {
+        packages.default = python.pkgs.buildPythonApplication {
+          pname = "s3dump";
+          version = "0.1.0";
+          format = "pyproject";
+          src = self;
+          propagatedBuildInputs = with python.pkgs; [ requests tqdm ];
+          nativeBuildInputs = with python.pkgs; [ setuptools wheel ];
+          checkInputs = with python.pkgs; [ pytest ];
+          doCheck = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            (python.withPackages (p: with p; [ requests tqdm pytest setuptools wheel ]))
+          ];
+        };
+      });
+}
